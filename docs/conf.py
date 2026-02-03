729db959bfa62d21d063a61c098ccceb25f78394
Andy Turner <A.G.D.Turner@gmail.com>
2026-02-03 11:11:46 +0000

    Set up for creating Sphinx documentation including adding Makefile target for gh-pages deployment

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# set of options see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'gabm'
+copyright = '2026, GABM contributors, University of Leeds'
+author = 'Andy Turner'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
